3693a7a6460eb5003b4292c53c75377e9e925812
wh1d33 <prosasa70@gmail.com>
2025-04-21 13:12:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,23 @@
-#include "../include/Book.h"
-#include "../include/Library.h"
 #include <iostream>
-using namespace std;
+#include "Library.h"
+
+int main() {
+    Library lib;
+
+    lib.addBook(Book("1984", "George Orwell", 1949, "1234567890"));
+    lib.addBook(Book("To Kill a Mockingbird", "Harper Lee", 1960, "0987654321"));
+
+    std::cout << "All books:\n";
+    lib.displayBooks();
+
+    std::cout << "\nSearch by author 'George':\n";
+    lib.searchByAuthor("George");
+
+    std::cout << "\nRemoving book with ISBN 1234567890\n";
+    lib.removeBook("1234567890");
+
+    std::cout << "\nAll books after removal:\n";
+    lib.displayBooks();
+
+    return 0;
+}
