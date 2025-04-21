407553ce5cdb662f1e4bd34b4037939e1f4b96a2
wh1d33 <prosasa70@gmail.com>
2025-04-21 13:12:15 +0300

    Update Book.cpp

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,47 +1,18 @@
 #include "../include/Book.h"
-#include <iostream>
-using namespace std;
 
-Book::Book(string title, string author, int year, string isbn)
+Book::Book(std::string title, std::string author, int year, std::string isbn)
     : title(title), author(author), year(year), isbn(isbn) {}
 
-string Book::getTitle() const {
-    return title;
-}
+std::string Book::getTitle() const { return title; }
+std::string Book::getAuthor() const { return author; }
+int Book::getYear() const { return year; }
+std::string Book::getISBN() const { return isbn; }
 
-string Book::getAuthor() const {
-    return author;
-}
-
-int Book::getYear() const {
-    return year;
-}
-
-string Book::getISBN() const {
-    return isbn;
-}
-
-void Book::setTitle(const string& title) {
-    this->title = title;
-}
-
-void Book::setAuthor(const string& author) {
-    this->author = author;
-}
-
-void Book::setYear(int year) {
-    this->year = year;
-}
-
-void Book::setISBN(const string& isbn) {
-    this->isbn = isbn;
-}
+void Book::setTitle(const std::string& newTitle) { title = newTitle; }
+void Book::setAuthor(const std::string& newAuthor) { author = newAuthor; }
+void Book::setYear(int newYear) { year = newYear; }
+void Book::setISBN(const std::string& newISBN) { isbn = newISBN; }
 
 bool Book::operator==(const Book& other) const {
     return isbn == other.isbn;
-}
-
-void Book::display() const {
-    std::cout << "Назва: " << title << ", Автор: " << author 
-              << ", Рік видання: " << year << ", ISBN: " << isbn << std::endl;
 }
